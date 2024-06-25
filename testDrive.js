848eca5c4a089ba2f0a56680f0e6e1f0afa8298c
Reverend <45483160+bubblobill@users.noreply.github.com>
2024-06-25 23:27:20 +0800

    Update testDrive.js

--- testDrive.js
+++ testDrive.js
@@ -1,3 +1,13 @@
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({ });
+
+const result = await octokit.request('GET /repos/bubblobill/soundShare/contents/audio', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  path: 'PATH',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+document.getElementById("response").innerText = result;
